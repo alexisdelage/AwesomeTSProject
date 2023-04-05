5a4d4a1ccbf2aa6d61d1a291adc23cfae1985712
Alexis Delage <alexisd@bam.tech>
2023-04-05 12:38:59 +0200

    create a red square with text hello

---
+++ src/components/HelloWorldSquare.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  square: {
+    width: 200,
+    aspectRatio: 1,
+    backgroundColor: '#A02132',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginBottom: 50,
+  },
+  text: {
+    color: '#70FF70',
+    fontWeight: '900',
+    fontSize: 50,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 'auto',
+    marginBottom: 'auto',
+  },
+});
+
+function HelloWorldSquare(): JSX.Element {
+  return (
+    <View style={styles.square}>
+      <Text style={styles.text}>Hello!</Text>
+    </View>
+  );
+}
+
+export default HelloWorldSquare;
