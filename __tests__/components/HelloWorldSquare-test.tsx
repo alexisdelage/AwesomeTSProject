ada709253d41cf98f909d2e28df71924f9ccebda
Alexis Delage <alexisd@bam.tech>
2023-04-05 16:52:49 +0200

    add a snpashot test

---
+++ __tests__/components/HelloWorldSquare-test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import HelloWorldSquare from '../../src/components/HelloWorldSquare';
+
+it('renders correctly', () => {
+  const tree = renderer.create(<HelloWorldSquare />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
