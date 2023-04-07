1cb329dcaf39f1bdb8a31e17c0e4f77a35801d2d
Alexis Delage <alexisd@bam.tech>
2023-04-07 14:48:09 +0200

    add tests

--- __tests__/components/BookList-test.tsx
+++ __tests__/components/BookList-test.tsx
@@ -1,10 +1,28 @@
+import 'react-native';
 import React from 'react';
-import renderer from 'react-test-renderer';
-import BookList from '../../src/components/BookList';
+import {render, screen, waitFor} from '@testing-library/react-native';
+import {QueryClientProvider, QueryClient} from '@tanstack/react-query';
+import nock from 'nock';
 
+import BookList from '../../src/components/BookList';
 import data from '../../data.json';
 
-it('renders correctly', () => {
-  const tree = renderer.create(<BookList data={data.docs} />).toJSON();
-  expect(tree).toMatchSnapshot();
+it('renders correctly', async () => {
+  // the query client
+  const queryClient = new QueryClient();
+  // replace the api call by mock data
+  const scope = nock('https://openlibrary.org')
+    .get('/search.json')
+    .query(true)
+    .reply(200, data);
+  // try to render the component
+  render(
+    <QueryClientProvider client={queryClient}>
+      <BookList />
+    </QueryClientProvider>,
+  );
+  // wait data is loaded
+  await waitFor(() => scope.done());
+  // check that the component did not change
+  expect(screen.toJSON()).toMatchSnapshot();
 });
