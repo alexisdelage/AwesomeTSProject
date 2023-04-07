c5bb12fa9cd0c7f15db51e36cb4e3f7a578bb28d
Alexis Delage <alexisd@bam.tech>
2023-04-07 14:48:04 +0200

    improve booklist

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,50 +1,50 @@
 import React, {useState} from 'react';
 import {FlatList, StyleSheet, Text} from 'react-native';
 import {useQuery} from '@tanstack/react-query';
-import axios, {AxiosResponse, AxiosError} from 'axios';
+import axios, {AxiosError} from 'axios';
 
 import BookListItem from './BookListItem';
 import {Book, Page} from '../../types';
 import * as styleConstants from '../../styles';
 
-/** component to display a Book List */
 function BookList() {
-  const [author, _] = useState('Jules Verne');
+  const [author, _] = useState('William Shakespeare');
 
-  const {isLoading, isError, data, error} = useQuery<
-    AxiosResponse<Page<Book>, any>,
-    AxiosError
-  >({
+  const bookQuery = useQuery<Page<Book>, AxiosError>({
     queryKey: ['book-list', author],
     queryFn: () =>
-      axios.get('https://openlibrary.org/search.json', {
-        params: {
-          author: author,
-          fields: ['title', 'key', 'author_name'],
-          limit: 50,
-          offset: 0,
-        },
-      }),
+      axios
+        .get<Page<Book>>('https://openlibrary.org/search.json', {
+          params: {
+            author: author,
+            fields: ['title', 'key', 'author_name'],
+            limit: 50,
+            offset: 0,
+          },
+        })
+        .then(res => res.data),
   });
 
-  if (isLoading) {
+  if (bookQuery.isLoading) {
     return <Text style={styles.textMessage}>Loading...</Text>;
   }
 
-  if (isError) {
+  if (bookQuery.isError) {
     return (
-      <Text style={styles.textMessage}>An error occurred: {error.message}</Text>
+      <Text style={styles.textMessage}>
+        An error occurred: {bookQuery.error.message}
+      </Text>
     );
   }
 
-  if (data.data.docs.length === 0) {
+  if (bookQuery.data.docs.length === 0) {
     return <Text style={styles.textMessage}>No results</Text>;
   }
 
   return (
     <FlatList
       contentContainerStyle={styles.listContainer}
-      data={data.data.docs}
+      data={bookQuery.data.docs}
       renderItem={item => <BookListItem book={item.item} />}
     />
   );
